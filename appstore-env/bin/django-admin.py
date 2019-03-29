8a65c03c40c8b07fd7024cb3fe7e78500dd8c2ea
hm-schroed7 <hm-schroed7@hm.edu>
2019-03-29 11:04:22 +0100

    new

---
+++ appstore-env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/DerAlex/Documents/Uni/Semester_4/SoftwareArchitektur/SA-Praktikum/appstore-env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
