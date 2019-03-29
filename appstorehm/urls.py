44be1f0fa5eeca8bf3caa049284c097b7a0e0a20
DerAlexx <alexweb199@hotmail.de>
2019-03-29 23:01:09 +0100

    newest version

---
+++ appstorehm/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    #path('admin/', admin.site.urls),
+    path('my/', admin.site.urls),
+    path('', views.index),
+    path('test/', views.test),
+    path('test2/', views.test2),
+    path('test2/login', views.login_)
+]
---
+++ users/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
+
+from .managers import CustomUserManager
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    TypeOfUserAccount = (
+    ('ST', 'Student'), #student
+    ('SA', 'Staff'), #staff
+    ('PR', 'Professor'), # prof
+    ('Fr', 'Externer Student'), #foreign
+    )
+
+    FK = (
+    ('FK01', 'Fakultät 01'),  
+    ('FK02', 'Fakultät 02'),  
+    ('FK03', 'Fakultät 03'),  
+    ('FK04', 'Fakultät 04'),  
+    ('FK05', 'Fakultät 05'),  
+    ('FK06', 'Fakultät 06'),  
+    ('FK07', 'Fakultät 07'),  
+    ('FK08', 'Fakultät 08'),  
+    ('FK09', 'Fakultät 09'),  
+    ('FK10', 'Fakultät 10'),  
+    ('FK11', 'Fakultät 11'),  
+    ('FK12', 'Fakultät 12'),  
+    ('FK13', 'Fakultät 13'),  
+    ('FK14', 'Fakultät 14'),  
+    )
+
+    email = models.EmailField(_('email address'), unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+    typOfAccount = models.CharField(
+        max_length=2,
+        choices=TypeOfUserAccount,
+        default='ST',
+    )
+    Fakultaet = models.CharField(
+        max_length=4,
+        choices=FK,
+        default='FK07',
+    )
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
