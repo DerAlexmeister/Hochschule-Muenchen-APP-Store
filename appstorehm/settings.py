4776374d1584d4f840a0fe2c5ceb80b20fc7f46a
DerAlexx <alexweb199@hotmail.de>
2019-04-01 12:44:15 +0200

    hshsh

--- appstorehm/settings.py
+++ appstorehm/settings.py
@@ -93,13 +93,20 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = '/staticfiles/'
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+#STATIC - Files 
+
+STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "/staticfiles/"),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles/')
 
+#STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
+#STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles/')
+
+
+#Media - Files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
