b32a9cea60c59a94162101a7491582ddcd10af02
DerAlexx <alexweb199@hotmail.de>
2019-04-01 20:53:05 +0200

    changes

--- restService/urls.py
+++ restService/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('apps/', views.AppListView.as_view()),
     path('apps/newest', views.newestAppsListView.as_view()),
     path('apps/oldest', views.oldestAppsListView.as_view()),
-    path('apps/newest', views.mostDownloadsListView.as_view()),
-    path('apps/oldest', views.tinyDownloadsListView.as_view()),
+    path('apps/mostdownloads', views.mostDownloadsListView.as_view()),
+    path('apps/lessdownloads', views.tinyDownloadsListView.as_view()),
 
     #User/Permission
     path('users/', views.UserListView)
