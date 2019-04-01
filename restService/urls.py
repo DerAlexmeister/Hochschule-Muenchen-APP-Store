eb178daa6629758409fd699d17a8d5e5cdc0c114
DerAlexx <alexweb199@hotmail.de>
2019-04-01 18:32:58 +0200

    changes

--- restService/urls.py
+++ restService/urls.py
@@ -5,8 +5,9 @@ from . import views
 urlpatterns = [
     #Apps
     path('apps/', views.AppListView.as_view()),
-    path('apps/newest', views.AppListView.as_view()),
-    
+    path('apps/newest', views.newestAppsListView.as_view()),
+    path('apps/oldest', views.oldestAppsListView.as_view()),
+
     #User/Permission
     path('users/', views.UserListView)
 
