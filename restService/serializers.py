92b5545b61db32dc6bcd5e1ea5bf9b144db178a3
DerAlexx <alexweb199@hotmail.de>
2019-04-01 16:48:13 +0200

    serilizers improvments

--- restService/serializers.py
+++ restService/serializers.py
@@ -4,6 +4,29 @@ from app_product import models
 class AppSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.APP
-        fields = ('appID', 'creator', '')
+        fields = ('appID', 
+            'creator', 
+            'appname', 
+            'body',
+            'downloads',
+            'keywords',
+            'createdAt',
+            'lastMod',
+            'website',
+            'contectEmail',
+            'sourcefiles',
+            'smallPic',
+            'bigimg',
+        )
 
     
+class CommentSerilzaer(serializers.ModelSerializer):
+    class Meta:
+        model = models.appKomments
+        fields = ('creator',
+            'post',
+            'title',
+            'kommentar',
+            'createdAt',
+            'lastMod'
+        )
