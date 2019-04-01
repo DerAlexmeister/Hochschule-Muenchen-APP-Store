2017c3553f50436f78734060f896ad9fa9303b2a
DerAlexx <alexweb199@hotmail.de>
2019-04-01 11:43:27 +0200

    newest version

--- appstorehm/urls.py
+++ appstorehm/urls.py
@@ -3,7 +3,6 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    #path('admin/', admin.site.urls),
     path('', views.index),
     path('admin/', admin.site.urls),
     path('api/user/', include('users.urls')),
