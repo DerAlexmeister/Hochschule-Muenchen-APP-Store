ae56f613562430e8b273e87babcf8d67348e279a
hm-schroed7 <hm-schroed7@hm.edu>
2019-04-03 16:53:34 +0200

    react 1

--- appstorehm/settings.py
+++ appstorehm/settings.py
@@ -50,7 +50,7 @@ ROOT_URLCONF = 'appstorehm.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['os.path.join('BASE_DIR, 'build')],
+        'DIRS': [os.path.join(BASE_DIR, 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
