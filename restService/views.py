66fc2180d5765046ef2c3b92f78c357c44104623
DerAlexx <alexweb199@hotmail.de>
2019-04-03 20:20:40 +0200

    changes

--- restService/views.py
+++ restService/views.py
@@ -43,34 +43,25 @@ class UserListView(generics.ListCreateAPIView):
 
 
 class newestAppsListView(generics.ListCreateAPIView):
-    http_method_names = ['GET']
+    http_method_names = ['get']
     queryset = appModel.objects.all().order_by('-createdAt')
     serializer_class = serializers.AppSerializer
 
 
 class oldestAppsListView(generics.ListCreateAPIView):
-    http_method_names = ['GET']
+    http_method_names = ['get']
     queryset = appModel.objects.all().order_by('createdAt')
     serializer_class = serializers.AppSerializer
 
 
 class mostDownloadsListView(generics.ListCreateAPIView):
-    http_method_names = ['GET']
+    http_method_names = ['get']
     queryset = appModel.objects.all().order_by('-downloads')
     serializer_class = serializers.AppSerializer
 
 
 class tinyDownloadsListView(generics.ListCreateAPIView):
-    http_method_names = ['GET']
+    http_method_names = ['get']
     queryset = appModel.objects.all().order_by('downloads')
     serializer_class = serializers.AppSerializer
 
-
-'''
-@api_view(['GET', 'POST'])
-def sortedByFakListView():
-    if request.method == 'POST':
-        
-    else:
-        queryset = appModel.objects.all().order_by('createdAt')
-        serializer_class = serializers.UserSerializer '''
