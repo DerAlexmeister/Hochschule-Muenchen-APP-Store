0e3229f5f04953f5b6cd1bf0fa6d9ca08abc9ccd
DerAlexx <alexweb199@hotmail.de>
2019-04-11 22:51:42 +0200

    last changes

--- appstorehm/urls.py
+++ appstorehm/urls.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 from django.urls import path,include, re_path
-from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    #path('', views.index),
     path('admin/', admin.site.urls),
     path('api/', include('restService.urls')),
     path('rest-auth/', include('rest_auth.urls')),
