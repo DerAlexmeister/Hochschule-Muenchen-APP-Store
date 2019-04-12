af3e1bf45adcf43366de58bff15e4d4b7ee149e7
hm-schroed7 <hm-schroed7@hm.edu>
2019-04-12 13:08:27 +0200

    react first try

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fb4a004f11b1882d96733015fb426052.js"
+  "/precache-manifest.5bcdaade4030ebef2d48d2d3466c0b1b.js"
 );
 
 workbox.clientsClaim();
