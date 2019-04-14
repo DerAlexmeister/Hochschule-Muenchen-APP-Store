fa3f4474f9ff6875e331a3d1d914830f6d849985
DerAlexx <alexweb199@hotmail.de>
2019-04-14 15:45:02 +0200

    app_details_page final

--- src/Cards.js
+++ src/Cards.js
@@ -3,12 +3,10 @@ import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-//import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import axios from 'axios';
 import { FaClock, FaArrowCircleRight} from 'react-icons/fa';
 import { Link } from 'react-router-dom'
-//import getBaseUrl from './consts.js'
 
 const styles = {
   card: {
