a9930555cda19ec6de6e4a8cd32673888832b4c6
DerAlexx <alexweb199@hotmail.de>
2019-04-14 22:53:19 +0200

    mockups

--- src/creatorsapps.js
+++ src/creatorsapps.js
@@ -103,13 +103,13 @@ class CreatorsApp extends React.Component{
 
     getype(it) {
         if (it === 'ST') {
-            return  <div>Student\in<FiUser style={{color:'#f10b51'}} /></div>
+            return  <div>Student\in <FiUser style={{color:'#f10b51'}} /></div>
         }
         else if (it === 'SA') {
             return  <div>Mitarbeiter\in <FiUser style={{color:'#f10b51'}} /></div>
         }
         else if (it === 'Fr') {
-            return  <div>Externer Student\in<FiUser style={{color:'#f10b51'}} /></div>
+            return  <div>Externer(e) Student\in <FiUser style={{color:'#f10b51'}} /></div>
         } else {
             return  <div>Professor\in <FiUser style={{color:'#f10b51'}} /></div>
         }
