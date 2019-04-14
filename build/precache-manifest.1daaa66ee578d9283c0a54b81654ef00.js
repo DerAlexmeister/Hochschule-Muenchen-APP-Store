b0357425e2d787ecef6710498926f29a4a0a47c4
DerAlexx <alexweb199@hotmail.de>
2019-04-14 15:46:31 +0200

    last version app_details_page

---
+++ build/precache-manifest.1daaa66ee578d9283c0a54b81654ef00.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa30025c1a700a242411f181ecf67665",
+    "url": "/static/media/logo_with_name.aa30025c.png"
+  },
+  {
+    "revision": "67b59c95cf394dadef84ef20fb4544b8",
+    "url": "/static/media/bemobil.67b59c95.jpg"
+  },
+  {
+    "revision": "b030aeec5f6435c3731848407da0ca00",
+    "url": "/static/media/banner1.b030aeec.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "78d02a3364708e29b30d",
+    "url": "/static/js/main.3029c862.chunk.js"
+  },
+  {
+    "revision": "bd9d836f71f162662366",
+    "url": "/static/js/2.70412fd5.chunk.js"
+  },
+  {
+    "revision": "78d02a3364708e29b30d",
+    "url": "/static/css/main.619b34da.chunk.css"
+  },
+  {
+    "revision": "bd9d836f71f162662366",
+    "url": "/static/css/2.b2d18d31.chunk.css"
+  },
+  {
+    "revision": "686cfc0985925a93705af23bc9994b86",
+    "url": "/index.html"
+  }
+];
