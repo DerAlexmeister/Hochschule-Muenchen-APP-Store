6cb4d32782681c481806079ed0999e2f9511c744
DerAlexx <alexweb199@hotmail.de>
2019-04-15 14:09:19 +0200

    Login Working token reciever fine

--- restService/views.py
+++ restService/views.py
@@ -249,7 +249,8 @@ def login(request):
     if not user:
         return Response({'error': 'Invalid Credentials'}, status=HTTP_404_NOT_FOUND)
     token, _ = Token.objects.get_or_create(user=user)
-    return Response({'token': token.key}, status=HTTP_200_OK)
+    print(token.key)
+    return JsonResponse({'token': token.key}, status=HTTP_200_OK)
 
 @csrf_exempt
 def app_list(request):
