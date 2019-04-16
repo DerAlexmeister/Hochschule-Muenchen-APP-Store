f3879d24f36627357063db83d67e79890592b4bf
DerAlexx <alexweb199@hotmail.de>
2019-04-16 20:22:01 +0200

    startseite final

--- src/App.js
+++ src/App.js
@@ -14,29 +14,32 @@ export default class App extends React.Component{
           <Bar/>
           <Header/>
           <SideNavPage/>
-            <div style={{backgroundColor:'#fff', width:'100%', height:'300px'}}>
-                  <h1 style={{textAlign: 'center', fontFamily: 'Montserrat', paddingTop: 110}}>Die einfachste Art die Zukunft vorherzusagen, ist sie zu implementieren.</h1>
-                  <h4 style={{textAlign: 'center', fontFamily: 'Montserrat'}}><i><b>David Heinemeier Hansson – (RoR)</b></i></h4>
+            <div style={{backgroundColor:'#282c34', width:'100%', height:'300px', borderBottom:'3px solid #f10b51', borderTop:'3px solid #f10b51'}}>
+                  <h1 style={{textAlign: 'center', fontFamily: 'Montserrat', paddingTop: 110, color:'#fff'}}>Die einfachste Art die Zukunft vorherzusagen, ist sie zu implementieren.</h1>
+                  <h4 style={{textAlign: 'center', fontFamily: 'Montserrat' , color:'#fff'}}><i><b>David Heinemeier Hansson – (RoR)</b></i></h4>
                   <br></br>
                 </div>
-                <div style={{backgroundColor:'#282c34', width:'100%', height:'500px'}}>
-                <h1 style={{textAlign: 'center', fontFamily: 'Montserrat', paddingTop: 30, color: '#fff'}}>Beliebteste Apps</h1>
-                <div style={{paddingTop: 40}}>
-                <SimpleSlider/>
-                </div>
+                <div style={{backgroundColor:'#282c34', width:'100%', height:'300px',borderBottom:'3px solid #f10b51'}}>
+                <br></br>
+                <h1 style={{textAlign: 'center', fontFamily: 'Montserrat', paddingTop: 30, color: '#fff'}}>Finde die richtige App für <span style={{color:'#f10b51'}}>dich</span></h1>
+                <ul style={{listStyle:'none', display:'flex', paddingTop: 50, justifyContent:'center'}}>
+                  <li style={{paddingLeft:0, paddingTop:0}}><a href="#" style={{paddingLeft:'45%',color:'#df0c0c', border:'2px solid #f10b51', borderRadius:4, width:'100%', height:'100%', textAlign:'center', margin:'auto', padding:15 }}> newest Apps </a></li>
+                  <li style={{paddingLeft:20, paddingTop:0}}><a href="#" style={{paddingLeft:'45%',color:'#df0c0c', border:'2px solid #f10b51', borderRadius:4, width:'100%', height:'100%', textAlign:'center', margin:'auto', padding:15 }}> older Apps </a></li>
+                  <li style={{paddingLeft:20, paddingTop:0}}><a href="#" style={{paddingLeft:'45%',color:'#df0c0c', border:'2px solid #f10b51', borderRadius:4, width:'100%', height:'100%', textAlign:'center', margin:'auto', padding:15 }}> most downloaded Apps </a></li>
+                  <li style={{paddingLeft:20, paddingTop:0}}><a href="#" style={{paddingLeft:'45%',color:'#df0c0c', border:'2px solid #f10b51', borderRadius:4, width:'100%', height:'100%', textAlign:'center', margin:'auto', padding:15 }}> less downloaded Apps </a></li>
+                </ul>
                 <br></br>
                 </div>
-                <div style={{backgroundColor:'#fff', width:'100%', height:'300px'}}>
-                  <br></br>
-                  <h1 style={{alignContent:'center'}}>Ausergewöhnliche Software überall</h1>
-                  <br></br>
-                </div>
                 <Bemobile />
-                <div style={{backgroundColor:'#fff', width:'100%', height:'300px'}}>
+                <div style={{backgroundColor:'#171a21', width:'100%', height:'300px', borderBottom:'3px solid #f10b51', borderTop:'3px solid #f10b51'}}>
+                <br></br>
+                  <ul style={{listStyle:'none'}}>
+                  <li><br></br><h1 style={{textAlign:'center', color:'#fff'}}>Du bist noch kein Mitglied</h1></li>
+                  <li><h3 style={{textAlign:'center', color:'#fff'}}>Registiere dich jetzt und lade dir die passende App für deine Altag herunter</h3><br></br><br></br></li>
+                  <li style={{paddingLeft:'45%'}}><a href='/signup' style={{paddingLeft:'45%',color:'#df0c0c', border:'2px solid #f10b51', borderRadius:4, width:'100%', height:'100%', textAlign:'center', margin:'auto', padding:15 }}>Jetzt Registieren</a></li>
+                  </ul>
                   <br></br>
-                  <h1 style={{alignContent:'center'}}>Ausergewöhnliche Software überall</h1>
-                  <br></br>
-            </div>
+              </div>
           <Footerown />
       </div>
     )
